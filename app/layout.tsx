c6d9fe0c836e134d911696a5e91b4530862e02b7
Devansh Jain <devanshdjboss@gmail.com>
2024-04-02 00:45:55 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Devansh Jain',
-  description: 'Developed by Devansh Jain',
+  title: "Devansh Jain",
+  description: "Developed by Devansh Jain",
   icons: {
-    icon: "/images/avatar.png"
-  }
-}
+    icon: "/images/avatar.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
